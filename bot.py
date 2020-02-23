9df865ea600cd646a9f25a53ab961a6efa313a2d
kp-void <contact@cody-p.me>
2020-02-22 20:25:59 -0800

    changed how bot works

--- bot.py
+++ bot.py
@@ -18,6 +18,27 @@ def locate_channel(guild, channelName):
 def remove_prefix(text, prefix):
     return text[text.startswith(prefix) and len(prefix):]
 
+def verify_dir(guild_id):
+    dirname = "./guilds/" + str(guild_id)
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+    return dirname
+
+def get_guild_emoji(guild_id):
+    filename = "./guilds/" + str(guild_id)
+    if os.path.exists(filename):
+        file = open(filename, "r")
+        
+        emoji = file.read()
+        print("retrieved emoji for server: " + emoji)
+        return emoji
+    else:
+        print("No entry for this server")
+
+    global EMOJI
+    return EMOJI
+
+
 #events ===================================================
 @client.event
 async def on_ready():
@@ -32,41 +53,53 @@ async def on_message(message):
     if message.author == client.user:
         return
     global EMOJI
-    if message.content.strip().lower() == EMOJI+"subscribe":
+
+    # the mention string doesn't handle nickname mentions
+    if message.content.startswith(client.user.mention) or message.content.startswith("<@!" + str(client.user.id) + ">"):
         if isinstance(message.channel, discord.DMChannel):
             await message.channel.send("I think this is a DM. You can't use this feature in DMs.")
         else:
-            dirname = "./guilds/" + str(message.channel.guild.id)
-            if not os.path.exists(dirname):
-                os.mkdir(dirname)
-            filename = dirname + "/" + str(message.author.id)
-            if os.path.exists(filename):
-                #already subscribed
-                os.remove(filename)
-                await message.channel.send("Unsubscribed you from this server.")
+            perms = message.author.permissions_in(message.channel)
+            if perms.manage_guild:
+                command = remove_prefix(remove_prefix(message.content, client.user.mention), "<@!" + str(client.user.id) + ">").strip()
+                filename = "./guilds/" + str(message.channel.guild.id)
+
+                if command.lower() == "reset":
+                    if os.path.exists(filename):
+                        os.remove(filename)
+                        await message.channel.send("Reset emoji for this server.")
+                    else:
+                        await message.channel.send("There is nothing to reset!")
+                elif len(command) > 0:
+                    file = open(filename, "w")
+                    file.write(command)
+                    file.close
+                    await message.channel.send("Set emoji on this server to " + command)
+                    print("Set emoji on " + str(message.channel.guild.id) + " to " + command)
+
+                else:
+                    await message.channel.send("I can't do anything with empty commands.")
             else:
-                open(filename, 'a').close()
-                await message.channel.send("Subscribed you on this server.")
+                await message.channel.send("You need Manage Server permission to do that.")
+
+    else:
+        print("\"" + message.content + "\" does not start with \"" + client.user.mention + "\"")
+        await message.channel.send(message.content + " does not start with " + client.user.mention)
 
 @client.event
 async def on_raw_reaction_add(data):
-    global EMOJI
-    if data.emoji.name == EMOJI:
-        filename = "./guilds/" + str(data.guild_id) + "/" + str(data.user_id)
-        if os.path.exists(filename):
-            guild = discord.utils.get(client.guilds, id=data.guild_id)
-            channel = discord.utils.get(guild.channels, id=data.channel_id)
-            message = await channel.fetch_message(data.message_id)
-            member = discord.utils.get(guild.members, id=data.user_id)
-            attachment_string = "**\n\nattachments:**"
-            for attachment in message.attachments:
-                attachment_string += '\n' + attachment.url
+    if data.emoji.name == get_guild_emoji(data.guild_id):
+        guild = discord.utils.get(client.guilds, id=data.guild_id)
+        channel = discord.utils.get(guild.channels, id=data.channel_id)
+        message = await channel.fetch_message(data.message_id)
+        member = discord.utils.get(guild.members, id=data.user_id)
+        attachment_string = "**\n\nattachments:**"
 
+        dm_channel = member.dm_channel
+        if dm_channel == None:
+            await member.create_dm()
             dm_channel = member.dm_channel
-            if dm_channel == None:
-                await member.create_dm()
-                dm_channel = member.dm_channel
-            await dm_channel.send("**==== NEW MESSAGE ====**\n" + message.content + attachment_string)
+        await dm_channel.send("**==== NEW MESSAGE ====**\n" + message.content + attachment_string)
 
 f=open("./token","r")
 token = f.read().strip()
